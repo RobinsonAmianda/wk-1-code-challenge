2a48f95d44103de680d5018e2dc546bb89ed6018
Robinson Amianda <robinsonamianda5@gmail.com>
2023-09-24 16:16:59 -0700

    initial assignment

---  Speed Detector.js
+++  Speed Detector.js
@@ -1,4 +1,45 @@
-let speed 
+let speed = 70
 if(speed <70){
 return `Ok`
 }
+else if(speed === 75){
+return `points:1`
+}
+else if(speed === 80){
+    return `points:2`
+}
+else if(speed === 85){
+    return `points:3`
+    }
+    else if(speed === 90){
+        return `points:4`
+        }
+        else if(speed === 95){
+            return `points:5`
+            }
+            else if(speed === 100){
+                return `points:6`
+                }
+                else if(speed === 105){
+                    return `points:7`
+                    }
+                    else if(speed === 110){
+                        return `points:8`
+                        }
+                        else if(speed === 115){
+                            return `points:9`
+                            }
+                            else if(speed === 120){
+                                return `points:10`
+                                }
+                                else if(speed === 125){
+                                    return `points:11`
+                                    }
+                                    else if(speed === 130){
+                                        return `points:12`
+                                        }
+                                        else {
+                                            return `license suspended`
+                                        }
+                                    
+                                        
